57c85378b36ca749959ed34293a9f2ea0fe838fe
Pranjal Shinde <110478058+pranjalls@users.noreply.github.com>
2024-12-04 11:39:47 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "C:/Users/HP/Downloads/Portfolio-master/src/Assets/Pranjal_Profile .pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+// Direct download link from Google Drive
+const pdf = "https://drive.google.com/uc?id=1kMEDASAtBhwT8GvV6966UR9Hi6bPee2X&export=download";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
@@ -19,6 +22,7 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
+        
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
